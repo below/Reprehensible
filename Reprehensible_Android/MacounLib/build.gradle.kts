669917a72e738f339bd9cb40d1a1188bbca33642
Alexander von Below <alexander.von_below@telekom.de>
2019-10-03 16:44:14 +0200

    Addded and created a podspec

--- Reprehensible_Android/MacounLib/build.gradle.kts
+++ Reprehensible_Android/MacounLib/build.gradle.kts
@@ -1,7 +1,18 @@
 plugins {
     kotlin("multiplatform")
+    id("org.jetbrains.kotlin.native.cocoapods")
 }
 
 kotlin {
     jvm ("android")
+}
+
+version = "1.0"
+
+kotlin {
+    cocoapods {
+        // Configure fields required by CocoaPods.
+        summary = "Macoun Sample Lib"
+        homepage = "www.macoun.de"
+    }
 }
