381a9f453a6884af1758fdc0d832e539ed774797
Alexander von Below <alexander.von_below@telekom.de>
2019-10-04 12:46:36 +0200

    Not working …

--- Reprehensible_Android/MacounLib/build.gradle.kts
+++ Reprehensible_Android/MacounLib/build.gradle.kts
@@ -10,9 +10,25 @@ kotlin {
 version = "1.0"
 
 kotlin {
+    val buildForDevice = project.findProperty("kotlin.native.cocoapods.target") == "ios_arm"
+    if (buildForDevice) {
+        iosArm64("iOS64")
+        iosArm32("iOS32")
+
+        val iOSMain by sourceSets.creating
+        sourceSets["iOS64Main"].dependsOn(iOSMain)
+        sourceSets["iOS32Main"].dependsOn(iOSMain)
+    } else {
+        iosX64("iOS")
+    }
+
     cocoapods {
         // Configure fields required by CocoaPods.
         summary = "Macoun Sample Lib"
         homepage = "www.macoun.de"
+    }
+
+    sourceSets["commonMain"].dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
     }
 }
